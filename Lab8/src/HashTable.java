38e1d95e4a81650f7b448e593960b72d6c5f4478
Alberto Hernández Lado <albertohljob@gmail.com>
2022-05-25 13:28:36 +0200

    Lab8

--- Lab8/src/HashTable.java
+++ Lab8/src/HashTable.java
@@ -1,29 +1,40 @@
 import java.util.LinkedList;
 import java.util.*;
 
-public class HashTable {
-    LinkedList<IWithName>[] arr;
+class HashTable
+{
+    private int threshold;
+    LinkedList<IWithName> arr[]; // use pure array
     private final static int defaultInitSize = 8;
     private final static double defaultMaxLoadFactor = 0.7;
     private int size;
     private final double maxLoadFactor;
-    private int threshold;
 
-    public HashTable(int initCapacity) {
+    public HashTable()
+    {
+        this(defaultInitSize);
+    }
+
+    public HashTable(int initCapacity)
+    {
         this(initCapacity, defaultMaxLoadFactor);
     }
 
 
     @SuppressWarnings("unchecked")
-    public HashTable(int initCapacity, double maxLF) {
-        if (initCapacity < 0) {
+    public HashTable(int initCapacity, double maxLF)
+    {
+        if (initCapacity < 0)
+        {
             throw new IllegalArgumentException("Illegal Capacity: " + initCapacity);
         }
-        if (maxLF <= 0 || Double.isNaN(maxLF)) {
+        if (maxLF <= 0 || Double.isNaN(maxLF))
+        {
             throw new IllegalArgumentException("Illegal Load: " + maxLF);
         }
 
-        if (initCapacity == 0) {
+        if (initCapacity == 0)
+        {
             initCapacity = 1;
         }
         this.maxLoadFactor = maxLF;
@@ -31,52 +42,56 @@ public class HashTable {
         threshold = (int) (initCapacity * maxLoadFactor);
     }
 
-    public boolean add(Object elem) {
-        if (!(elem instanceof IWithName)) {
+    public boolean add(Object elem)
+    {
+        if (!(elem instanceof IWithName))
+        {
             return false;
         }
-
         IWithName value = (IWithName) elem;
 
-        if (size >= threshold) {
+        if (size >= threshold)
+        {
             doubleArray();
         }
 
         int index = (value.hashCode() & 0x7FFFFFFF) % arr.length;
-
-        if (arr[index] == null) {
+        if (arr[index] == null)
+        {
             arr[index] = new LinkedList<>();
         }
-
-        if (arr[index].contains(value)) {
+        if (arr[index].contains(value))
+        {
             return false;
         }
-
         arr[index].add(value);
         size++;
-
         return true;
     }
 
     @SuppressWarnings("unchecked")
     private void doubleArray()
     {
-        int oldCap = arr.length;
-        int newCap = oldCap * 2;
+        int oldCapacity = arr.length;
+        int newCapacity = oldCapacity * 2;
 
         LinkedList<IWithName>[] oldArr = arr;
-        arr = new LinkedList[newCap];
+        arr = new LinkedList[newCapacity];
 
-        threshold = (int) (newCap * maxLoadFactor);
+        threshold = (int) (newCapacity * maxLoadFactor);
 
-        for (int i = 0; i < oldCap; i++) {
+        for (int i = 0; i < oldCapacity; i++)
+        {
             List<IWithName> old = oldArr[i];
-            if (old == null) {
+            if (old == null)
+            {
                 continue;
             }
-            for (IWithName e : old) {
-                int index = (e.hashCode() & 0x7FFFFFFF) % newCap;
-                if (arr[index] == null) {
+            for (IWithName e : old)
+            {
+                int index = (e.hashCode() & 0x7FFFFFFF) % newCapacity;
+                if (arr[index] == null)
+                {
                     arr[index] = new LinkedList<>();
                 }
                 arr[index].add(e);
@@ -86,17 +101,22 @@ public class HashTable {
 
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++)
+        {
             result.append(i).append(':');
             LinkedList<IWithName> list = arr[i];
-            if (list != null) {
+            if (list != null)
+            {
                 Iterator<IWithName> it = list.iterator();
-                while (it.hasNext()) {
+                while (it.hasNext())
+                {
                     IWithName elem = it.next();
                     result.append(' ').append(elem.getName());
-                    if (it.hasNext()) {
+                    if (it.hasNext())
+                    {
                         result.append(',');
                     }
 
@@ -107,17 +127,22 @@ public class HashTable {
         return result.toString();
     }
 
-    public Object get(Object toFind) {
-        if (toFind == null) {
+    public Object get(Object toFind)
+    {
+        if (toFind == null)
+        {
             return null;
         }
         int index = (toFind.hashCode() & 0x7FFFFFFF) % arr.length;
         LinkedList<IWithName> list = arr[index];
-        if (list == null) {
+        if (list == null)
+        {
             return null;
         }
-        for (IWithName e : list) {
-            if (toFind.equals(e)) {
+        for (IWithName e : list)
+        {
+            if (toFind.equals(e))
+            {
                 return e;
             }
         }
